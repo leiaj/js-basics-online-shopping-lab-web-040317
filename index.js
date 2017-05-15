7add7c242be459a05c6d006ea373bb5b31c688d8
github username <github email address>
2017-05-15 17:46:20 -0400

    yay

--- index.js
+++ index.js
@@ -9,22 +9,58 @@ function setCart(c) {
 }
 
 function addToCart(item) {
- // write your code here
+let price = Math.floor(Math.random() * 100)
+cart.push({[item]: price})
+console.log(`${item} has been added to your cart.`)
+return cart
 }
 
 function viewCart() {
-  // write your code here
-}
+  let newArray = []
+  let cartLength = cart.length
+  if (cartLength === 0) {
+    console.log("Your shopping cart is empty.")
+    }
+  for (var i = 0; i < cart.length; i++) {
+   let cartObject = cart[i]
+    let item = Object.keys(cartObject)[0]
+    let price = cartObject[item]
+    let blah = `${item} at $${price}`
+    newArray.push(blah)
+    }
+    console.log(`In your cart, you have ${newArray.join(", ")}.`)
+  }
+
 
 function total() {
-  // write your code here
+  let totalPrice = 0
+  for (var i = 0; i < cart.length; i++) {
+   let cartObject = cart[i]
+   let item = Object.keys(cartObject)[0]
+   let price = cartObject[item]
+   totalPrice += price
+  }
+  return totalPrice
 }
 
 function removeFromCart(item) {
-  // write your code here
+
+  for (var i = 0; i < cart.length; i++) {
+    let itemName = Object.keys(cart[i])[0]
+    if (itemName === item) {
+    cart.splice(i - 1, 1)
+    return cart
+    }
+  }
+  console.log("That item is not in your cart.")
 }
 
 function placeOrder(cardNumber) {
-  // write your code here
-}
+  if (typeof cardNumber === "undefined"){
+    console.log("We don't have a credit card on file for you to place your order.")
+  } else {
+    console.log(`Your total cost is $${total()}, which will be charged to the card ${cardNumber}.`)
+  }
+  cart = []
 
+}
